9962326367fcec209ac923c4dbe4cdfb79a786f1
Hzqskywkr <142490934+Hzqskywkr@users.noreply.github.com>
2023-12-28 20:07:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from NPSolver.MaxcutSolver import MaxcutSolver
 from NPSolver.QKPSolver import QKPSolver
+from NPSolver.QAPSolver import QAPSolver
 from NPSolver.TSPSolver import TSPSolver
 from NPSolver.CVRPSolver import CVRPSolver
 from NPSolver.SATSolver import SATSolver
+
 #example for using maxcut solver
 #maxcut_solver = MaxcutSolver("./maxcut_instances/w63x63_8bit.csv")
 #best_vector, max_cut_edge =maxcut_solver.solver()
 #print('max_cut_edge',max_cut_edge)
 
 #example for using qkp solver
-qkp_solver = QKPSolver("./qkp_instances/r_10_100_1.txt",2)
-S_best, value, weight = qkp_solver.solver()
-print('S_best',S_best)
-print('Total value',value)
-print('Total weight', weight)
+#qkp_solver = QKPSolver("./qkp_instances/r_10_100_1.txt",2)
+#S_best, value, weight = qkp_solver.solver()
+#print('S_best',S_best)
+#print('Total value',value)
+#print('Total weight', weight)
+
+#example for using qkp solver
+qap_solver = QAPSolver("./qap_instances/nug8.dat")
+permut, min_cost = qap_solver.solver()
+print('min cost',min_cost)
+print('permut', permut)
 
 #example for using tsp solver
 #tsp_solver = TSPSolver("./tsp_instances/burma14.xml","./tsp_instances/burma14.tsp")
